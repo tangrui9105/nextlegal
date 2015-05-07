53b21ebc737592c33bedd106e2061ce92aea8128
tangrui <tangrui9105@gmail.com>
2015-05-07 23:11:29 +0800

    Add sample code

---
+++ src/main/java/com/nextlegal/backend/Application.java
@@ -0,0 +1,11 @@
+package com.nextlegal.backend;
+
+/**
+ * Created by tangrui on 2015/5/7.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
